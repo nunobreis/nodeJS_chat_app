07d8b6b16b80abe7eb28adfa6963b650e3ab7e17
nunobreis <nuno@catalpa.io>
2018-04-23 15:46:32 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Joana Moura',
-    text: 'Compra o bilhete do Primavera Sound'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage', {
-    from: 'Nuno Reis',
-    text: 'Yup',
-    createdAt: 123123
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('You got a new message:', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', (socket) => {
