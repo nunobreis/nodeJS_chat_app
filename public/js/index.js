fea85447f2661c6c79b60629eecc25f81abe5ac2
nunobreis <nuno@catalpa.io>
2018-04-24 12:05:08 +0200

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,21 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
+socket.on('newMessage', (message) => {
   console.log('New Message', message);
+  const li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', (event) => {
+  event.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, () => {
+
+  });
 });
